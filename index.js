395a32e3d6a16230ee396f77286db1c00d0c01dc
sivaprabhakar <sivaprabhakaran.94@gmail.com>
2024-02-29 21:30:08 +0530

    cors is add in backend

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import dotenv from "dotenv"
 import AppRoutes from './src/routes/index.js'
+import cors from 'cors'
 dotenv.config()
 
 const PORT = process.env.PORT
 const app = express();
-
+app.use(cors())
 app.use(express.json())
 app.use('/',AppRoutes)
 
